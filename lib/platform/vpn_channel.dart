397ebd9fb9c372003193a0bc0674403551c89985
Sanuyyq <134251520+Sanuyyq@users.noreply.github.com>
2026-04-26 23:52:33 +0600

    Delete lib directory

--- lib/platform/vpn_channel.dart
+++
@@ -1,4 +0,0 @@
-// Legacy facade: используется оставшимися ссылками.
-// Новый код должен использовать VpnHost (см. vpn_host.dart / platform_vpn_host.dart).
-export 'platform_vpn_host.dart';
-export 'vpn_host.dart';
